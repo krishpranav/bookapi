16dbac2e762c0802522486cb777e057184096351
krishpranav <krisna.pranav@gmail.com>
2021-12-30 13:14:11 +0530

    controllers: books[createbookinput,updatebookinput]

---
+++ controllers/books.go
@@ -0,0 +1,25 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/krishpranav/bookapi/models"
+)
+
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func FindBooks(c *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{"data": books})
+}
