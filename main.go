8df9b23c9d846c393691e592f78911117b1f5b00
krishpranav <krisna.pranav@gmail.com>
2021-12-30 15:42:39 +0530

    update pkg

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/krishpranav/bookapi/controller"
 	"github.com/krishpranav/bookapi/models"
 )
-
-// let check db is working
 
 func main() {
 	r := gin.Default()
 
 	models.ConnectDatabase()
+
+	/* routes */
+	r.GET("/books", controller.FindBooks)
 
 	r.Run()
 }
